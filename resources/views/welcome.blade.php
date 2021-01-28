d18695f40b323049814561d92ab5810fd2081794
cowik <cowike@gmail.com>
2021-01-28 21:35:54 +0700

    edit view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Cowik
+                    Laravel Cowik Berhasil Bikin Deployer Laravel
                 </div>
 
                 <div class="links">
